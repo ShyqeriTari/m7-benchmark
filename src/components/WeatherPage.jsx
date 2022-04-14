3d0e7f0b4bb94872946b2314f9063ade134c7f33
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-04-14 12:42:57 +0200

    Update WeatherPage.jsx

--- src/components/WeatherPage.jsx
+++ src/components/WeatherPage.jsx
@@ -11,12 +11,12 @@ const WeatherPage = () => {
     const getWeather = async() => {
         try {
             
-            const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${params.cityName}&limit=1&appid=${process.env.API_KEY}`)
+            const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${params.cityName}&limit=1&appid=${process.env.REACT_APP_API_KEY}`)
             const data = await response.json()
 
             if(response.ok){
 
-                const weatherResp = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${process.env.API_KEY}`)
+                const weatherResp = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${process.env.REACT_APP_API_KEY}`)
                 if(weatherResp.ok){
 
                 const weatherData = await weatherResp.json()
